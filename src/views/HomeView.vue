6c255c24f507a2a89abb419424b7ce3b391b3e5d
VinzJ <kejoxz@gmail.com>
2025-05-27 06:03:48 +0800

    chore(setup): setup tailwind & add custom color pallete

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <div id="titleContainer" class="bg-primary p-4 w-fit rounded-2xl">
+      <h1 class="text-on-primary text-4xl font-bold bg-onSurface">
+        🧑‍💻 Vue Playground
+      </h1>
+    </div>
+    <p class="text-primary/75 text-xl">
+      Welcome to my Vue.js learning project. This page showcases the exercises
+      and components I've built while learning Vue.js. Use the menu above to
+      explore each feature I've practiced so far.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
